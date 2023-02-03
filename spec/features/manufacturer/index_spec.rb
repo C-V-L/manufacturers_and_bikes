4789d46f7a0733fe0b7d53315367cdacd83d50c4
Conner Van Loan <cdvanloan@gmail.com>
2023-02-03 08:28:46 -0700

    Adds user story 9/10 tests and passes

--- spec/features/manufacturer/index_spec.rb
+++ spec/features/manufacturer/index_spec.rb
@@ -34,4 +34,22 @@ RSpec.describe 'Manufacturer Index' do
       expect(@special.name).to appear_before(@allcity.name)
     end
   end
+
+  describe 'user story 9 & 10' do
+    it 'displays a link to the child index page from every page' do
+      @surly = Manufacturer.create!(name: "Surly", employees:76, domestic: true, created_at: Time.now - 1.hour)
+      @trucker = @surly.bikes.create!(name: "Disc Trucker", wheelsize: 700, carbon: false)
+      @monkey = @surly.bikes.create!(name: "Karate Monkey", wheelsize: 650, carbon: false)
+      @bigblock = @surly.bikes.create!(name: "Big Block", wheelsize: 275, carbon: false)
+
+      visit "/manufacturers/"
+      expect(page).to have_link("Bikes")
+      expect(page).to have_link("Manufacturers")
+
+      visit "/bikes/"
+      expect(page).to have_link("Manufacturers")
+      expect(page).to have_link("Bikes")
+
+    end
+  end
 end
