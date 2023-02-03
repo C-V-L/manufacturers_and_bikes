eb9fe39332b01e28f47d5ab5a68eb57b9804af4b
Conner Van Loan <cdvanloan@gmail.com>
2023-02-03 07:56:48 -0700

    Adds #bike_count test

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -9,19 +9,29 @@ RSpec.describe Manufacturer, type: :model do
     it {should have_many :bikes}
   end
 
-  describe '#sort_by_created_at' do
-    # User Story 6, Parent Index sorted by Most Recently Created 
-    # As a visitor
-    # When I visit the parent index,
-    # I see that records are ordered by most recently created first
-    # And next to each of the records I see when it was created
+  describe 'user story 6' do
+    describe '#sort_by_created_at' do
+      it 'sorts the records by the most recent created_at timestamp' do
+        @surly = Manufacturer.create!(name: "Surly", employees:76, domestic: true)
+        @special = Manufacturer.create!(name: "Specialized", employees: 1701, domestic: false, created_at: Time.now - 1.hour)
+        @allcity = Manufacturer.create!(name: "All City", employees: 25, domestic: true, created_at: Time.now - 1.day)
+        
+        expect(Manufacturer.sort_by_created_at.to_a).to eq([@surly,@special,@allcity ])
+      end
 
-    it 'sorts the records by the most recent created_at timestamp' do
-      @surly = Manufacturer.create!(name: "Surly", employees:76, domestic: true)
-      @special = Manufacturer.create!(name: "Specialized", employees: 1701, domestic: false, created_at: Time.now - 1.hour)
-      @allcity = Manufacturer.create!(name: "All City", employees: 25, domestic: true, created_at: Time.now - 1.day)
-      
-      expect(Manufacturer.sort_by_created_at.to_a).to eq([@surly,@special,@allcity ])
+    end
+  end
+
+  describe 'user story 7' do
+    describe '#bike_count' do
+      it 'returns the number of bikes the manufacturer has' do
+        @surly = Manufacturer.create!(name: "Surly", employees:76, domestic: true, created_at: Time.now - 1.hour)
+        @trucker = @surly.bikes.create!(name: "Disc Trucker", wheelsize: 700, carbon: false)
+        @monkey = @surly.bikes.create!(name: "Karate Monkey", wheelsize: 650, carbon: false)
+        @bigblock = @surly.bikes.create!(name: "Big Block", wheelsize: 275, carbon: false)
+
+        expect(@surly.bike_count).to eq(3)
+      end
     end
   end
 
